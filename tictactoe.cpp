98330bbafd9dbb2aa24c0f37c789139ac853c2d1
bugybon <bugybon1@gmail.com>
2025-11-25 12:36:11 +0200

    judge funtion is added, but not working yet

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,23 +2,111 @@
 #include <string>
 
 enum Tile {
-	_,
-	O,
-	X
+	X = 1,
+	_ = 0,
+	O = -1
 };
+
+int valueArray[9];
+
+int maxValue(Tile* game, int& alpha, int& betta, const int& depth, const Tile& whose_turn, const bool& isFirstIter);
+int minValue(Tile* game, int& alpha, int& betta, const int& depth, const Tile& whose_turn, const bool& isFirstIter);
 
 bool isTerminalState(Tile* game) {
 	for (int i = 0; i < 3; i++) {
-		if ((game[i * 3] != _ && game[i * 3] == game[i * 3 + 1] && game[i * 3] == game[i * 3 + 2]) ||
-			(game[i] != _ && game[i] == game[3 + i] && game[i] == game[6 + i]))
+		if ((game[i * 3] != Tile::_ && game[i * 3] == game[i * 3 + 1] && game[i * 3] == game[i * 3 + 2]) ||
+			(game[i] != Tile::_ && game[i] == game[3 + i] && game[i] == game[6 + i]))
 			return true;
+	}
+
+	if ((game[0] != Tile::_ && game[0] == game[4] && game[0] == game[8]) ||
+		(game[3] != Tile::_ && game[2] == game[4] && game[2] == game[6]))
+		return true;
+
+	for(int i = 0; i < 9; i++)
+		if(game[i] == Tile::_)return false;
+
+	return true;
+}
+
+int terminalStateValue(Tile* game, const int& depth) {
+	for (int i = 0; i < 3; i++) {
+		if (game[i * 3] != _ && game[i * 3] == game[i * 3 + 1] && game[i * 3] == game[i * 3 + 2])
+			return (game[i * 3] == Tile::O) ? (depth - 10) : (10 - depth);
+		if (game[i] != _ && game[i] == game[3 + i] && game[i] == game[6 + i])
+			return (game[i] == Tile::O) ? (depth - 10) : (10 - depth);
 	}
 
 	if ((game[0] != _ && game[0] == game[4] && game[0] == game[8]) ||
 		(game[3] != _ && game[2] == game[4] && game[2] == game[6]))
-		return true;
+		return (game[4] == Tile::O) ? (depth - 10) : (10 - depth);
 
-	return false;
+	return 0;
+}
+
+void judge(Tile game[], const Tile& whose_turn, const int& depth) {
+	int value, alpha = INT8_MIN, betta = INT8_MAX;
+
+	if (whose_turn == X) {
+		value = maxValue(game, alpha, betta, depth, whose_turn, true);
+	}
+	else {
+		value = minValue(game, alpha, betta, depth, whose_turn, true);
+
+	}
+	
+
+	for (int i = 0; i < 9; i++) {
+		if (valueArray[i] == value)
+			std::cout << i / 3 + 1 << " " << i % 3 + 1 << std::endl;
+	}
+}
+
+int maxValue(Tile* game, int& alpha, int& betta, const int& depth, const Tile& whose_turn, const bool& isFirstIter) {
+	if (isTerminalState(game)) 
+		return terminalStateValue(game, depth);
+
+	int value = INT8_MIN;
+
+	for (int i = 0; i < 9; i++) {
+		if (game[i] != Tile::_)
+			continue;
+
+		game[i] = whose_turn;
+		value = std::max(value, minValue(game, alpha, betta, depth + 1, (whose_turn == Tile::X) ? Tile::O : Tile::X, false));
+		game[i] = Tile::_;
+		if (value >= betta) {
+			if (isFirstIter)
+				valueArray[i] = value;
+			return value;
+		}
+		alpha = std::max(alpha, value);
+	}
+
+	return value;
+}
+
+int minValue(Tile* game, int& alpha, int& betta, const int& depth, const Tile& whose_turn, const bool& isFirstIter) {
+	if (isTerminalState(game))
+		return terminalStateValue(game, depth);
+
+	int value = INT8_MAX;
+
+	for (int i = 0; i < 9; i++) {
+		if (game[i] != Tile::_)
+			continue;
+
+		game[i] = whose_turn;
+		value = std::min(value, maxValue(game, alpha, betta, depth + 1, (whose_turn == Tile::X) ? Tile::O : Tile::X,false));
+		game[i] = Tile::_;
+		if (value <= alpha) {
+			if (isFirstIter) valueArray[i] = value;
+			return value;
+		}
+		betta = std::min(betta, value);
+	}
+
+	return value;
 }
 
 Tile translateCharToTile(const char& ch) {
@@ -31,11 +119,11 @@ Tile translateCharToTile(const char& ch) {
 		return _;
 	default:
 		std::cout << "Char not a tile";
-		return;
+		return _;
 	}
 }
 
-void translateStateToArray(const std::string& state, Tile* game) {
+void translateStateToGame(const std::string& state, Tile* game) {
 	for (int i = 0; i < 9; i++) {
 		game[i] = translateCharToTile(state[i]);
 	}
@@ -48,8 +136,6 @@ std::string translateInputToState(const std::string& input) {
 		if(input[i] == '_' || input[i] == 'O' || input[i] == 'X')
 			tempState += input[i];
 	}
-
-	if (tempState.size() != 9) std::cout << "Wrong state input";
 
 	return tempState;
 }
@@ -66,25 +152,30 @@ void printState(Tile* game) {
 
 int main() {
 	std::string inputType, inputTurn, inputState, input;
-	Tile game[9];
+	Tile game[9], buffer[9];
 	int depth = 0;
 	
 	std::cin >> inputType;
+	std::getline(std::cin, inputTurn); //clear the remaining line from cin >>
 	std::getline(std::cin, inputTurn);
 	for (int i = 0; i < 7; i++) {
 		std::getline(std::cin, input);
 		inputState += translateInputToState(input);
 	}
-	translateStateToArray(inputState, game);
+
+	if (inputState.size() != 9) std::cout << "Wrong state input";
+
+	translateStateToGame(inputState, game);
+	translateStateToGame(inputState, buffer);
 
 	for (int i = 0; i < 9; i++)
 		depth += (game[i] != _) ? 1 : 0;
 
 	if(inputType == "JUDGE"){
 		if (inputTurn[5] == 'X') {
-			judge(game, X, depth);
+			judge(buffer, X, depth);
 		} else if (inputTurn[5] == 'O') {
-			judge(game, O, depth);
+			judge(buffer, O, depth);
 		}
 		else {
 			std::cout << "Not a possible turn";
